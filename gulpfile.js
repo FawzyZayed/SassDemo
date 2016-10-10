a0babaa99fca7d6c8d463ad249723e1077ed4b78
fawzy@outlook.com <fawzy.zayed@edigits.net>
2016-10-10 12:34:44 +0200

    install gulp

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+'use strict';
+var gulp = require ('gulp');
+var sass = require('glup-sass');
+// SCSS to CSS Converter 
+gulp.task('sass', function () {
+  return gulp.src('SassDemo/assets/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('SassDemo/assets/css'));
+});
+// Watch task for SCSS
+gulp.task('sass:watch', function () {
+  gulp.watch('SassDemo/assets/*.scss', ['sass']);
+});
