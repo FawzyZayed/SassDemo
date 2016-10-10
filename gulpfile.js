c291d95cc28066ff6286137610bd347b97b8659e
fawzy@outlook.com <fawzy.zayed@edigits.net>
2016-10-10 12:41:18 +0200

    add SCSS file

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 'use strict';
 var gulp = require ('gulp');
 var sass = require('glup-sass');
+
 // SCSS to CSS Converter 
 gulp.task('sass', function () {
   return gulp.src('SassDemo/assets/*.scss')
